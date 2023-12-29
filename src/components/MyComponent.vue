f3e08c4be6b3c208093be0c1c3f0a5380d9bddd9
Daniél Garrido <danielgarrido7@hotmail.com>
2023-12-29 13:26:08 +0100

    Init

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+export default {
+    data() {
+        return { res: null }
+    },
+    methods: {
+        async click() {
+            this.res = await fetch('http://localhost:8080/employees').then(response => {
+                // The API call was successful!
+                return response.json();
+            }).catch(function (err) {
+                // There was an error
+                console.warn('Something went wrong.', err);
+                return null;
+            });
+        }
+    }
+}
+</script>
+
+<template>
+    <button type="button" @click="click()">Click me</button>
+    <p>{{ res }}</p>
+</template>
